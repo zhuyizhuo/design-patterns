0b00dd0f144c76cb7aa38fb044d9aabefceecc33
zhuyizhuo <zhu.yizhuo@163.com>
2018-05-30 07:37:15 +0800

    add
    add

--- src/main/java/com/yizhuo/learning/patterns/adapter/TestAdapter.java
+++ src/main/java/com/yizhuo/learning/patterns/adapter/TestAdapter.java
@@ -5,6 +5,8 @@ package com.yizhuo.learning.patterns.adapter;
  */
 public class TestAdapter {
     public static void main(String[] args) {
+        //BankCardJsonAdapter  将接口返回类型由对象适配成json  简单例子
+//         也可以适配成其他对象或者其他格式
         BankCardJsonAdapter bankCardJsonAdapter = new BankCardJsonAdapter();
         String s = bankCardJsonAdapter.queryBankCardMsg("111");
         System.out.println(s);
