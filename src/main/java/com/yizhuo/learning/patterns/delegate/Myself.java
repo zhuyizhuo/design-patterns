87eb4fda94704e2d98f4ad370c2898ac4df5c85c
zhuyizhuo <zhu.yizhuo@163.com>
2018-05-30 07:34:01 +0800

    add
    add

--- src/main/java/com/yizhuo/learning/patterns/delegate/Myself.java
+++ src/main/java/com/yizhuo/learning/patterns/delegate/Myself.java
@@ -5,6 +5,7 @@ package com.yizhuo.learning.patterns.delegate;
  */
 public class Myself {
     public static void main(String[] args) {
+        //我选择快递公司发快递  快递公司根据派发地不同 委派不同快递员去具体派发
         ExpressCompany expressDelivery = new ExpressCompany();
         expressDelivery.sendExpress("朝阳");
     }
